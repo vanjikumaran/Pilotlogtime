d9f095eae58db277ca5f9a57e6f1bc34c231f77e
Sivajothy <vsivajothy@lm-sfa-00871979.corp.ebay.com>
2015-08-11 15:52:18 -0700

    Created maven project

---
+++ src/main/java/lk/everbloom/App.java
@@ -0,0 +1,13 @@
+package lk.everbloom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
